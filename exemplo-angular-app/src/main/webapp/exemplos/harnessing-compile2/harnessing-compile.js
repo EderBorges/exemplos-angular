2111aa54dd065eaa754752ef4037985ceacb233b
eder-bd <eder-bd@segplan.go.gov.br>
2016-06-06 15:04:53 -0300

    adicionando menu, configurando js quebrado

--- exemplo-angular-app/src/main/webapp/exemplos/harnessing-compile2/harnessing-compile.js
+++ exemplo-angular-app/src/main/webapp/exemplos/harnessing-compile2/harnessing-compile.js
@@ -1,17 +1,16 @@
 angular.module("harnessing", [])
     .controller("HarnessingCtrl", function ($scope) {
-    
+
 //     $scope.console = [{value:"teste"},{value:"teste2"}];
 
         $scope.config = [
-            {nome: "userName", tipo: "campo"},
-            {nome: "email", tipo: "campo"},
-            {nome: "serie", tipo: "campo"},
-            {nome: "matricula", tipo: "campo"},
+            {id: "userName", nome: "Nome de Usuário", tipo: "campo"},
+            {id: "email", nome: "E-mail", tipo: "campo"},
+            {id: "serie", nome: "Série", tipo: "campo"},
+            {id: "matricula",nome: "Matricula", tipo: "campo"},
         ];
-   
-    
-    
+
+
         $scope.aluno = {
             id: "123"
             , userName: "Ederbd"
@@ -19,7 +18,6 @@ angular.module("harnessing", [])
             , serie: "1º ano"
             , matricula: "213132"
         };
-
 
 
     })
@@ -48,39 +46,45 @@ angular.module("harnessing", [])
                     var tpl = '';
                     type = type || defaultType;
                     switch (type) {
-                    case "ini":
-                        $scope.config.forEach(function(item,index,arr){
-                            tpl = tpl + '<div debug="value.'+item.nome+'" type="'+item.tipo+'"></div>';
-                        });
-                        break;     
-                    case "campo":
-                            tpl = tpl + '<input ng-model="value"></input>{{value}}';
-                        break;                            
-                    case "json":
+                        case "ini":
+                            if ($scope.config) {
+                                $scope.config.forEach(function (item) {
+                                    tpl = tpl + '<div debug="value.' + item.id + '" type="' + item.tipo + '" t-name="' + item.nome + '"></div>';
+                                });
+                            }
+                            break;
+                        case "campo":
+                            tpl = tpl + '<label><h6>'+$attrs.tName+'</h6></label>';
+                            tpl = tpl + '<input ng-model="value"></input>';
+                            break;
+                        case "json":
                             tpl = tpl + '<pre ng-bind="value | json"> </pre>';
-                        break;
-                    case "type":
-                        tpl = '<pre> Type : {{value | type}} </pre>';
-                        break;
-                    case "array":
-                        var innerType = $attrs.innerType || defaultType;
-                        tpl = '<div ng-repeat="el in value" debug="el" type="' + innerType + '">{{el}}</div>';
-                        break;
-                    case "full":
-                        tpl = '<div debug="value" type="type"></div><div debug="value" type="json"></div>';
-                        break;
+                            break;
+                        case "type":
+                            tpl = '<pre> Type : {{value | type}} </pre>';
+                            break;
+                        case "array":
+                            var innerType = $attrs.innerType || defaultType;
+                            tpl = '<div ng-repeat="el in value" debug="el" type="' + innerType + '">{{el}}</div>';
+                            break;
+                        case "full":
+                            tpl = '<div debug="value" type="type"></div><div debug="value" type="json"></div>';
+                            break;
 
-                    default:
-                        tpl = '<b>Tipo desconhecido "' + type + '" de </b><pre> {{value}} </pre>';
-                        break;
+                        default:
+                            tpl = '<b>Tipo desconhecido "' + type + '" de </b><pre> {{value}} </pre>';
+                            break;
 
-                    };
+                    }
+                    ;
                     $compile(tpl)($scope, function (el) {
                         $element.html('').append(el);
                     });
                 };
                 $attrs.$observe('type', setContent);
-                setContent();
+                if (!$attrs.type) {
+                    setContent();
+                }
 
             }
             , scope: {
